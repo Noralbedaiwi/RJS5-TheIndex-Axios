cfb61ebdc30bf9e9092199c97d618bb2f34382e3
Nora <n.albedaiwi@hotmail.com>
2019-01-22 19:29:23 +0300

    submission

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import authors from "./data.js";
-
 // Components
 import Sidebar from "./Sidebar";
 import SearchBar from "./SearchBar";
 import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
+import Loading from "./Loading";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentAuthor: {},
-      filteredAuthors: []
+      filteredAuthors: [],
+      authors: [],
+      loading: true
     };
     this.selectAuthor = this.selectAuthor.bind(this);
     this.unselectAuthor = this.unselectAuthor.bind(this);
     this.filterAuthors = this.filterAuthors.bind(this);
   }
 
-  selectAuthor(author) {
-    this.setState({ currentAuthor: author });
+  selectAuthor(authorID) {
+    axios
+      .get(`https://the-index-api.herokuapp.com/api/authors/${authorID}`)
+      .then(res => res.data)
+      .then(incomingAuthor =>
+        this.setState({
+          currentAuthor: incomingAuthor,
+          loading: false
+        })
+      );
   }
 
   unselectAuthor() {
@@ -31,8 +40,10 @@ class App extends Component {
 
   filterAuthors(query) {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
-      return `${author.first_name} ${author.last_name}`.includes(query);
+    let filteredAuthors = this.state.authors.filter(author => {
+      return `${author.first_name} ${author.last_name}`
+        .toLowerCase()
+        .includes(query);
     });
     this.setState({ filteredAuthors: filteredAuthors });
   }
@@ -40,6 +51,8 @@ class App extends Component {
   getContentView() {
     if (this.state.currentAuthor.first_name) {
       return <AuthorDetail author={this.state.currentAuthor} />;
+    } else if (this.state.loading) {
+      return <Loading />;
     } else {
       return (
         <AuthorsList
@@ -49,6 +62,19 @@ class App extends Component {
         />
       );
     }
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://the-index-api.herokuapp.com/api/authors/")
+      .then(res => res.data)
+      .then(incomingAuthors =>
+        this.setState({
+          authors: incomingAuthors,
+          filteredAuthors: incomingAuthors,
+          loading: false
+        })
+      );
   }
 
   render() {
